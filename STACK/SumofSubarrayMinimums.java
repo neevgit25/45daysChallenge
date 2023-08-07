ec84c1fcf8e83dc216b51bbdab1601fdcd5bfd89
Nivedita Kar <115336952+neevgit25@users.noreply.github.com>
2023-08-07 23:14:29 +0530

    Add files via upload

---
+++ STACK/SumofSubarrayMinimums.java
@@ -0,0 +1,72 @@
+package STACK;
+
+import java.util.Stack;
+
+public class SumofSubarrayMinimums {
+    
+     int mod=(int)Math.pow(10,9)+7;
+    public class Pair{
+        int e;
+        int c;
+        public Pair(int e, int c){
+            this.e = e;
+            this.c = c;
+        }
+    }
+    public int sumSubarrayMins(int[] arr) {
+        //BRUTEFORCE APPROCH tc=O(N)
+       /* int sum=0;
+         for(int i=0;i<arr.length;i++)
+         {
+             int min=arr[i];
+             for(int k=i;k<arr.length;k++)
+             {
+                 if(min>arr[k])
+                     min=arr[k];
+
+                 sum+=min;
+             }
+         }
+         return sum;
+         */
+            int n =arr.length;
+        int[] left = getLeftMin(arr, n);
+        int[] right = getRightMin(arr, n);
+        long sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum = (sum+ (long)arr[i]*left[i]*right[i])%mod;
+        }
+        return (int)sum;
+    }
+     public int[] getLeftMin(int[] arr, int n) {
+        int[] a = new int[n];
+        Stack<Pair> s = new Stack<>();
+        for(int i = 0; i< n; i++){
+            int count = 1;
+            while(!s.isEmpty() && s.peek().e > arr[i]){
+                count+=s.peek().c;
+                s.pop();
+            }
+            s.push(new Pair(arr[i], count));
+            a[i] = count;
+        }
+        return a;   
+    }
+    
+    public int[] getRightMin(int[] arr, int n) {
+        int[] a = new int[n];
+        Stack<Pair> s = new Stack<>();
+        for(int i = n-1; i>=0; i--){
+            int count = 1;
+            while(!s.isEmpty() && s.peek().e >= arr[i]){
+                count+=s.peek().c;
+                s.pop();
+            }
+            s.push(new Pair(arr[i], count));    
+            a[i] = count;  
+        }
+        return a;
+    }
+}
+
+
